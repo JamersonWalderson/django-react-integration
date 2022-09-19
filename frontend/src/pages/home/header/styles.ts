558e503f62214d42653e637a6f06b1ce90b74356
jamesw <jamersonwalderson@gmail.com>
2022-09-19 09:08:36 -0300

    chore: removendo importações desncessárias

--- frontend/src/pages/home/header/styles.ts
+++ frontend/src/pages/home/header/styles.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import Background from '../../../assets/images/background.jpg'
 import { device } from "../../../utils/device";
 
 export const Container = styled.div`
