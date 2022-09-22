21159cdea0588e15b7901eda275837bba638589b
jamesw <jamersonwalderson@gmail.com>
2022-09-22 18:47:18 -0300

    feat: api funcional

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
+from django.urls import path, include
+from rest_framework import routers
+from api import serializers
+
+route = routers.DefaultRouter()
+
+route.register(r'portfolio', serializers.ProfileViewSet, basename='Portfolio')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', include(route.urls)),
 ]
